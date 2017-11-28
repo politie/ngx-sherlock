478147a3c386eca4b5d9c7683b73fffcfed592e6
Wouter Spaak <w.spaak@gmail.com>
2017-11-28 11:37:06 +0100

    Wouter/auto change detect (#13)
    * Making the library publishable to NPM.
    
    * Adding documentation to README.md.
    
    * Adding autoDetectChanges function.
    
    * adding autoDetectChanges
    
    * Added a test to make sure CDR is no longer triggered after destruction.
    
    * Angular integration testing instead of (too simplified) functional unit testing.
    
    * Updated README.md based on comments.
    
    * Simplifying README.md

--- README.md
+++ README.md
@@ -28,9 +28,77 @@ import { NgxSherlockModule } from '@politie/ngx-sherlock';
 export class AppModule { }
 ```
 
+### `autoDetectChanges`
+
+**Signature**:
+```typescript
+autoDetectChanges(detectorRef: ChangeDetectorRef): void;
+```
+
+The function `autoDetectChanges` will automatically run a change detection cycle when `Derivables` or `DerivableProxies` within a component's template change internal state. The function should be called in an `OnInit` lifecycle hook of a component or directive.
+
+The `autoDetectChanges` function guarantees model and view fidelity, meaning one can easily use Angular's forms and template functionality.
+
+#### Example
+
+`trusty-sidekick.component.ts`:
+```typescript
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Input } from '@angular/core';
+import { autoDetectChanges } from '@politie/ngx-sherlock';
+import { atom } from '@politie/sherlock';
+import { ProxyDescriptor } from '@politie/sherlock-proxy';
+
+@Component({
+    selector: 'trusty-sidekick',
+    template: `
+        <input type="text" [(ngModel)]="sidekick$.firstname.$value" placeholder="First name">
+        <input type="text" [(ngModel)]="sidekick$.surname.$value" placeholder="Surname">
+        <sidekick-greeter [name]="sidekick$"></sidekick-greeter>
+    `,
+})
+export class TrustySidekickComponent {
+    readonly sidekick$ = new ProxyDescriptor().$create(atom({firstname: 'John', surname: 'Watson'}));
+}
+
+@Component({
+    selector: 'sidekick-greeter',
+    template: `
+        <h2 *ngIf="!beObnoxious">Well hello there, {{name.firstname.$value}} {{name.surname.$value}}!</h2>
+        <h2 *ngIf="beObnoxious">So good of you to finally join us, {{name.surname.$value}}...</h2>
+        
+        <button (click)="toggle()">Change mood</button>     
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidekickGreeterComponent implements OnInit {
+    @Input() name: DerivableProxy<{ firstname: string, surname: string }>;
+    obnoxious$ = atom(false);
+
+    get beObnoxious() {
+        return this.obnoxious$.get();
+    }
+
+    constructor(private readonly cdr: ChangeDetectorRef) { }
+
+    ngOnInit() {
+        // Here we call #autoDetectChanges which will automatically react on changes in the state of
+        // SidekickGreeterComponent#name.
+        autoDetectChanges(this.cdr);
+    }
+
+    toggle() {
+        this.obnoxious$.swap(mood => !mood);
+    }
+}
+```
+
+---
+
 ### `ValuePipe`
 
-The `ValuePipe` unwraps a `Derivable` or `DerivableProxy` value and triggers the `ChangeDetectorRef` whenever an internal value changes and a change detection cycle is needed.
+The `ValuePipe` unwraps a `Derivable` or `DerivableProxy` value and triggers the `ChangeDetectorRef` whenever an internal value changes
+and a change detection cycle is needed. This allows a component to have an `OnPush` `ChangeDetectionStrategy`, greatly increasing
+performance.
 
 #### Example
 
@@ -42,18 +110,19 @@ The `ValuePipe` unwraps a `Derivable` or `DerivableProxy` value and triggers the
 
 `my.component.ts`:
 ```typescript
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Atom, atom } from '@politie/sherlock';
 
 @Component({
     selector: 'my-component';
     templateUrl: 'my.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MyComponent implements OnInit {
     readonly counter$: Atom<number> = atom(0);
 
     ngOnInit() {
-        setInterval(() => counter$.swap(i => i++), 1000);
+        setInterval(() => this.counter$.swap(i => i++), 1000);
     }
 }
 ```
---
+++ src/change-detection/auto-detect-changes.spec.ts
@@ -0,0 +1,143 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { atom } from '@politie/sherlock';
+import { ProxyDescriptor } from '@politie/sherlock-proxy';
+import { autoDetectChanges } from './auto-detect-changes';
+
+describe('change detection', () => {
+    describe('autoDetectChanges', () => {
+        let fixture: ComponentFixture<TestCpt>;
+        let component: TestCpt;
+        let detectChangesSpy: jasmine.Spy;
+        let onDestroySpy: jasmine.Spy;
+
+        beforeEach(() => {
+            TestBed.configureTestingModule({ imports: [CommonModule], declarations: [TestCpt] }).compileComponents();
+        });
+
+        beforeEach(() => {
+            fixture = TestBed.createComponent(TestCpt);
+            component = fixture.componentInstance;
+            detectChangesSpy = spyOn(component.cdr, 'detectChanges').and.callThrough();
+            onDestroySpy = spyOn(component.cdr as any, 'onDestroy').and.callThrough();
+
+            fixture.detectChanges();
+        });
+
+        it('should throw when no ChangeDetectorRef is provided', () => {
+            expect(() => autoDetectChanges({} as any)).toThrowError(`autoDetectChanges was not called with a valid ChangeDetectorRef.`);
+        });
+
+        it('should have registered a reaction destroying function with the ChangeDetectorRef', () => {
+            expect(onDestroySpy).toHaveBeenCalledTimes(1);
+            // tslint:disable-next-line:ban-types
+            const destroyer: Function = onDestroySpy.calls.mostRecent().args[0];
+            expect(typeof destroyer).toEqual('function');
+            expect(destroyer.name).toEqual('done');
+        });
+
+        it('should have called ChangeDetectorRef#detectChanges once on initialization', () => {
+            expect(detectChangesSpy).toHaveBeenCalledTimes(1);
+        });
+
+        describe('using Derivable', () => {
+            // let's reset detectChangesSpy so we start out with zero calls.
+            beforeEach(() => detectChangesSpy.calls.reset());
+
+            it('should correctly render state on initialization', () => {
+                expect(getH1Text(fixture)).toContain(component.state$.get());
+            });
+
+            it('should call ChangeDetectorRef#detectChanges when state changes', () => {
+                component.state$.set('new value');
+                expect(detectChangesSpy).toHaveBeenCalledTimes(1);
+                expect(getH1Text(fixture)).toContain('new value');
+
+                component.state$.set('newer value');
+                expect(detectChangesSpy).toHaveBeenCalledTimes(2);
+                expect(getH1Text(fixture)).toContain('newer value');
+            });
+
+            it('should stop reaction when component gets destroyed', () => {
+                component.state$.set('foo');
+                expect(detectChangesSpy).toHaveBeenCalledTimes(1);
+
+                // Destroy component
+                fixture.destroy();
+
+                component.state$.set('bar');
+                expect(detectChangesSpy).toHaveBeenCalledTimes(1);
+            });
+        });
+
+        describe('using DerivableProxy', () => {
+            // Set component to 'proxy mode' and reset detectChangesSpy call count.
+            beforeEach(() => {
+                component.useProxy$.set(true);
+                detectChangesSpy.calls.reset();
+            });
+
+            it('should correctly render state on initialization', () => {
+                expect(getH1Text(fixture)).toBe(component.state$.derive(v => v.toUpperCase()).get());
+            });
+
+            it('should correctly render when underlying atom changes', () => {
+                component.state$.set('foobar');
+                expect(getH1Text(fixture)).toBe('FOOBAR');
+                expect(detectChangesSpy).toHaveBeenCalledTimes(1);
+            });
+
+            it('should correctly render when $value is set and call ChangeDetectorRef#detectChanges', () => {
+                component.proxyState$.$value = 'Set via $value';
+                expect(getH1Text(fixture)).toBe('SET VIA $VALUE');
+                expect(detectChangesSpy).toHaveBeenCalledTimes(1);
+
+                component.proxyState$.$value = 'new value for $value';
+                expect(getH1Text(fixture)).toBe('NEW VALUE FOR $VALUE');
+                expect(detectChangesSpy).toHaveBeenCalledTimes(2);
+            });
+
+            it('should stop detecting changes when component gets destroyed', () => {
+                component.proxyState$.$value = 'foo';
+                expect(detectChangesSpy).toHaveBeenCalledTimes(1);
+
+                fixture.destroy();
+
+                component.proxyState$.$value = 'bar';
+                expect(detectChangesSpy).toHaveBeenCalledTimes(1);
+            });
+        });
+    });
+});
+
+@Component({
+    template: `
+    <h1 *ngIf="!useProxy">{{state}}</h1>
+    <h1 *ngIf="useProxy">{{proxyState$.$value}}</h1>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+class TestCpt implements OnInit {
+    readonly state$ = atom('my value');
+    readonly proxyState$ = new ProxyDescriptor().$create(this.state$.lens({
+        get: v => v.toUpperCase(),
+        set: v => v.toLowerCase(),
+    }));
+
+    readonly useProxy$ = atom(false);
+
+    get state() { return this.state$.get(); }
+    get useProxy() { return this.useProxy$.get(); }
+
+    constructor(readonly cdr: ChangeDetectorRef) { }
+
+    ngOnInit() {
+        autoDetectChanges(this.cdr);
+    }
+}
+
+function getH1Text(fixture: ComponentFixture<TestCpt>): string {
+    return fixture.debugElement.query(By.css('h1')).nativeElement.textContent.trim();
+}
