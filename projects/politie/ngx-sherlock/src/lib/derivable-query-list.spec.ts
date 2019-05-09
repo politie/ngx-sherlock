2fd7496d7d0156f55e8b2419de7f69b347a2ecd0
Yasin Kocak <>
2019-05-09 15:23:20 +0200

    unit test

--- projects/politie/ngx-sherlock/src/lib/derivable-query-list.spec.ts
+++ projects/politie/ngx-sherlock/src/lib/derivable-query-list.spec.ts
@@ -6,9 +6,9 @@ describe('DerivableQueryList', () => {
 
     @Component({
         template: `
-            <p #p *ngIf="first">First</p>
-            <p #p *ngIf="second">Second</p>
-            <p #p *ngIf="third">Third</p>
+            <p #item *ngIf="first">First</p>
+            <p #item *ngIf="second">Second</p>
+            <p #item *ngIf="third">Third</p>
         `,
     })
     class TestComponent {
@@ -16,7 +16,7 @@ describe('DerivableQueryList', () => {
         second = false;
         third = false;
 
-        @ViewChildren('p')
+        @ViewChildren('item')
         private readonly paragraphs!: QueryList<ElementRef<HTMLParagraphElement>>;
         readonly paragraphs$ = derivableQueryList(() => this.paragraphs);
     }
@@ -34,6 +34,7 @@ describe('DerivableQueryList', () => {
 
     it('resolve after initialized', () => {
         expect(componentInstance.paragraphs$.resolved).toBeFalsy();
+        componentInstance.paragraphs$.markInitialized();
         componentInstance.paragraphs$.markInitialized();
         expect(componentInstance.paragraphs$.resolved).toBeTruthy();
     });
@@ -55,5 +56,22 @@ describe('DerivableQueryList', () => {
         componentInstance.third = false;
         fixture.detectChanges();
         expect(componentInstance.paragraphs$.get().length).toEqual(0);
+
+    });
+
+    it('derive the viewChildrens when used inside a derivation', () => {
+        let paragraphs: any;
+        componentInstance.paragraphs$.react(v => paragraphs = v);
+
+        componentInstance.paragraphs$.markInitialized();
+        expect(paragraphs.length).toEqual(1);
+
+        componentInstance.second = true;
+        fixture.detectChanges();
+        expect(paragraphs.length).toEqual(2);
+
+        componentInstance.third = true;
+        fixture.detectChanges();
+        expect(paragraphs.length).toEqual(3);
     });
 });
