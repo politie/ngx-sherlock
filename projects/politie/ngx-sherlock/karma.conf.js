5a905cf049faa4336f4405d524fcccc6d5e56348
Yasin Kocak <y.kocak@xyn.nl>
2021-12-21 13:33:29 +0100

    removed deprecated karma-coverage-istanbul-reporter and installed karma-coverage

--- projects/politie/ngx-sherlock/karma.conf.js
+++ projects/politie/ngx-sherlock/karma.conf.js
@@ -8,14 +8,14 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
+            require('karma-coverage'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
+        coverageReporter: {
             dir: require('path').join(__dirname, '../../../coverage'),
             reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true
