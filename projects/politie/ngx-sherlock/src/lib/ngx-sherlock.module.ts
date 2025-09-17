aababd2a4fd39631289e3041d833d2f8fa74d48c
Web Artisan <985902+yasinkocak@users.noreply.github.com>
2025-09-17 13:09:29 +0200

    Update ngx-sherlock.module.ts
    Added ValueForPipe in ngx-sherlock.module.ts

--- projects/politie/ngx-sherlock/src/lib/ngx-sherlock.module.ts
+++ projects/politie/ngx-sherlock/src/lib/ngx-sherlock.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ValuePipe } from './value.pipe';
+import { ValueForPipe } from './value-for.pipe';
 
 @NgModule({
     declarations: [
         ValuePipe,
+        ValueForPipe,
     ],
     exports: [
         ValuePipe,
+        ValueForPipe,
     ],
 })
 export class NgxSherlockModule { }
