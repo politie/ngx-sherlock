84748522676f1d0f9806432779e9791d11bbb7de
Yasin Kocak <985902+yasinkocak@users.noreply.github.com>
2025-09-18 09:12:02 +0200

    Added valueForPipe to module and public-api

--- projects/politie/ngx-sherlock/src/lib/ngx-sherlock.module.ts
+++ projects/politie/ngx-sherlock/src/lib/ngx-sherlock.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { ValuePipe } from './value.pipe';
 import { ValueForPipe } from './value-for.pipe';
+import { ValuePipe } from './value.pipe';
 
 @NgModule({
     declarations: [
-        ValuePipe,
         ValueForPipe,
+        ValuePipe,
     ],
     exports: [
-        ValuePipe,
         ValueForPipe,
+        ValuePipe,
     ],
 })
 export class NgxSherlockModule { }
